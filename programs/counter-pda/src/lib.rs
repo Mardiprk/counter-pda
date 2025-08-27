127ab7ba35eee7cbcb58cbf9729eb921032d9d26
Mardiprk <mk1659770@gmail.com>
2025-08-27 14:06:03 +0000

    PDA

--- programs/counter-pda/src/lib.rs
+++ programs/counter-pda/src/lib.rs
@@ -6,11 +6,61 @@ declare_id!("FZvcmfjG5CJS1h3CKc9Ro3fBSbNCSnnZWK5EEG6vfbY1");
 pub mod counter_pda {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
+        let counter = &mut _ctx.accounts.counter;
+        counter.value = 0;
+        msg!("Counter initialized to 0");
+        Ok(())
+    }
+
+    pub fn increment(_ctx: Context<Update>) -> Result<()> {
+        let counter = &mut _ctx.accounts.counter;
+        counter.value += 1;
+        msg!("Counter incremented, New Value: {}", counter.value);
+        Ok(())
+    }
+
+    pub fn decrement(_ctx: Context<Update>) -> Result<()> {
+        let counter = &mut _ctx.accounts.counter;
+        counter.value -= 1;
+        msg!("Counter decremented, New Value: {}", counter.value);
+        Ok(())
+    }
+
+    pub fn reset(_ctx: Context<Update>) -> Result<()> {
+        let counter = &mut _ctx.accounts.counter;
+        counter.value = 0;
+        msg!("Counter reset to 0");
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(
+        init,
+        payer = user,
+        space = 8 + 8,
+        seeds = [b"counter".as_ref(), user.key().as_ref()],
+        bump
+    )]
+    pub counter: Account<'info, Counter>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Update<'info> {
+    #[account(
+        mut,
+        seeds = [b"counter".as_ref(), user.key().as_ref()],
+        bump
+    )]
+    pub counter: Account<'info, Counter>,
+    pub user: Signer<'info>,
+}
+#[account]
+pub struct Counter {
+    pub value: i64,
+}
