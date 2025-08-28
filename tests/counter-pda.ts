04cc0e1371b2d92eff22eb43313e6791fa0eb364
Mardiprk <mk1659770@gmail.com>
2025-08-28 09:33:11 +0000

    pda

--- tests/counter-pda.ts
+++ tests/counter-pda.ts
@@ -1,16 +1,125 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { CounterPda } from "../target/types/counter_pda";
+import { PublicKey } from "@solana/web3.js";
 
 describe("counter-pda", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.counterPda as Program<CounterPda>;
+  const wallet = provider.wallet;
+  console.log("Using wallet:", wallet.publicKey.toBase58());
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  //PDA
+  const [counterPda, bump] = PublicKey.findProgramAddressSync(
+    [Buffer.from("counter"), wallet.publicKey.toBuffer()],
+    program.programId
+  );
+
+  console.log("Counter PDA: ", counterPda.toBase58());
+
+
+
+  it("✔️ Initialialize Counter", async () => {
+    try{
+      const tx = await program.methods.initialize().accounts({
+        counter: counterPda,
+        user: wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId
+      }).rpc();
+
+      console.log("✔️ Initialize Tx:", tx);
+      
+      const account = program.account.counter.fetch(counterPda);
+      console.log("Counter Value:", (await account).value);
+    }catch(err){
+      console.log("Already Initialized or Error:", err);
+    }
+  })
+
+  //second increment - val: 1
+  it("✔️ Increment Counter", async () => {
+    try{
+      const tx = await program.methods.increment().accounts({
+        counter: counterPda,
+        user: wallet.publicKey
+      }).rpc();
+
+      console.log("✔️ Increment Tx:", tx);
+
+      const account = program.account.counter.fetch(counterPda);
+      console.log("Counter Value:", (await account).value);
+    }catch(err){
+      console.log("Already Initialized or Error:", err);
+    }
+  });
+
+  //second increment - val: 2
+  it("✔️ Increment Counter", async () => {
+    try{
+      const tx = await program.methods.increment().accounts({
+        counter: counterPda,
+        user: wallet.publicKey
+      }).rpc();
+
+      console.log("✔️ Increment Tx:", tx);
+
+      const account = program.account.counter.fetch(counterPda);
+      console.log("Counter Value:", (await account).value);
+    }catch(err){
+      console.log("Already Initialized or Error:", err);
+    }
+  });
+
+  //sthird increment - val: 3
+  it("✔️ Increment Counter", async () => {
+    try{
+      const tx = await program.methods.increment().accounts({
+        counter: counterPda,
+        user: wallet.publicKey
+      }).rpc();
+
+      console.log("✔️ Increment Tx:", tx);
+
+      const account = program.account.counter.fetch(counterPda);
+      console.log("Counter Value:", (await account).value);
+    }catch(err){
+      console.log("Already Initialized or Error:", err);
+    }
+  });
+
+  //decrement - val: 2
+  it("✔️ Decrement Counter", async () => {
+    try{
+      const tx = await program.methods.decrement().accounts({
+        counter: counterPda,
+        user: wallet.publicKey
+      }).rpc();
+      console.log("✔️ Decrement Tx:", tx);
+
+      const account = program.account.counter.fetch(counterPda);
+      console.log("Counter Value:", (await account).value);
+    }catch(err){
+      console.log("Already Initialized or Error:", err);
+    }
+
+  });
+
+  //reset counter - val: 0
+  it("✔️ Reset Counter", async () => {
+    try{
+      const tx = await program.methods.reset().accounts({
+        counter: counterPda,
+        user: wallet.publicKey
+      }).rpc();
+      console.log("✔️ Reset Tx:", tx);
+
+      const account = program.account.counter.fetch(counterPda);
+      console.log("Counter Value:", (await account).value);
+    }catch(err){
+      console.log("Already Initialized or Error:", err);
+    }
+
   });
 });
